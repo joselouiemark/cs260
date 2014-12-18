a7768d4629a84eb72eb313a0b19863f91611c929
Jose Louie Mark <joselouiemark@gmail.com>
2014-12-18 21:17:34 +0800

    Homepage unit test

--- cs260/urls.py
+++ cs260/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cs260.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+	# Examples:
+	url(r'^$', 'todolists.views.home_page', name='home'),
+	# url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
 )
+
