cd1a5e02c400965ab1c4cbd495b75df25deddd69
Jose Louie Mark <joselouiemark@gmail.com>
2014-12-20 03:05:33 +0800

    Added unit tests for adding, editing and querying(by date and status) todos

--- todolists/tests.py
+++ todolists/tests.py
@@ -158,4 +158,128 @@ class HomePageTest(TestCase):
 		self.assertEqual(third_saved_todo.summary, 'sample summary three')
 		self.assertEqual(third_saved_todo.status, 3)
 		
+	def test_saving_and_retrieving_todos_with_filter(self):
+		#tests listing of all todos with filtering
+		#first create records, specifically three of them (a waiting, a cancelled and a finished)
 		
+		form1 = Todo()
+		form1.title = 'sample title waiting'
+		form1.summary = 'sample summary waiting'
+		form1.date = '2014-12-29'
+		form1.status = 1
+		form1.save()
+		
+		form2 = Todo()
+		form2.title = 'sample title finished'
+		form2.summary = 'sample summary finished'
+		form2.date = '2014-12-29'
+		form2.status = 2
+		form2.save()
+		
+		form3 = Todo()
+		form3.title = 'sample title cancelled'
+		form3.summary = 'sample summary cancelled'
+		form3.date = '2014-12-29'
+		form3.status = 3
+		form3.save()
+		
+		savedTodos = Todo.objects.all()
+		
+		allObjsFilterWaiting = savedTodos.filter(status=1)
+		self.assertEqual(allObjsFilterWaiting.count(), 1)
+		self.assertEqual(allObjsFilterWaiting[0].title, 'sample title waiting')
+		self.assertEqual(allObjsFilterWaiting[0].summary, 'sample summary waiting')
+		self.assertEqual(allObjsFilterWaiting[0].status, 1)
+		
+		allObjsFilterFinished = savedTodos.filter(status=2)
+		self.assertEqual(allObjsFilterFinished.count(), 1)
+		self.assertEqual(allObjsFilterFinished[0].title, 'sample title finished')
+		self.assertEqual(allObjsFilterFinished[0].summary, 'sample summary finished')
+		self.assertEqual(allObjsFilterFinished[0].status, 2)
+		
+		allObjsFilterCancelled = savedTodos.filter(status=3)
+		self.assertEqual(allObjsFilterCancelled.count(), 1)
+		self.assertEqual(allObjsFilterCancelled[0].title, 'sample title cancelled')
+		self.assertEqual(allObjsFilterCancelled[0].summary, 'sample summary cancelled')
+		self.assertEqual(allObjsFilterCancelled[0].status, 3)
+		
+	def test_saving_and_retrieving_todos_with_date_filter(self):
+		#tests listing of all todos regardless of the type
+		#first create records, specifically three of them (from dec 21, dec 22, dec 23)
+		
+		form1 = Todo()
+		form1.title = 'sample title waiting'
+		form1.summary = 'sample summary waiting'
+		form1.date = '2014-12-21'
+		form1.status = 1
+		form1.save()
+		
+		form2 = Todo()
+		form2.title = 'sample title finished'
+		form2.summary = 'sample summary finished'
+		form2.date = '2014-12-22'
+		form2.status = 2
+		form2.save()
+		
+		form3 = Todo()
+		form3.title = 'sample title cancelled'
+		form3.summary = 'sample summary cancelled'
+		form3.date = '2014-12-23'
+		form3.status = 3
+		form3.save()
+		
+		savedTodos = Todo.objects.all()
+		
+		#we want to get only betweeb dec 22 and dec 23
+		dfrom = '20141222'
+		dto = '20141223'
+		savedTodos = savedTodos.filter(date__range=[ datetime.strptime(dfrom, '%Y%m%d' ),datetime.strptime(dto, '%Y%m%d' )])
+
+		self.assertEqual(savedTodos.count(), 2)
+		self.assertEqual(savedTodos[0].title, 'sample title finished')
+		self.assertEqual(savedTodos[0].summary, 'sample summary finished')
+		self.assertEqual(savedTodos[0].status, 2)
+		self.assertEqual(savedTodos[1].title, 'sample title cancelled')
+		self.assertEqual(savedTodos[1].summary, 'sample summary cancelled')
+		self.assertEqual(savedTodos[1].status, 3)
+		
+	def test_saving_and_retrieving_with_after_date_modification(self):
+		#tests listing of todos
+		#modifies dates of all waiting todos that are before today
+		#first create records, specifically three of them (from dec 21, dec 22, dec 23)
+		#create 3 waiting todos
+		form1 = Todo()
+		form1.title = 'sample title one'
+		form1.summary = 'sample summary one'
+		form1.date = '2014-12-21'
+		form1.status = 1
+		form1.save()
+		
+		form2 = Todo()
+		form2.title = 'sample title two'
+		form2.summary = 'sample summary two'
+		form2.date = '2014-12-22'
+		form2.status = 1
+		form2.save()
+		
+		form3 = Todo()
+		form3.title = 'sample title three'
+		form3.summary = 'sample summary three'
+		form3.date = '2014-12-23'
+		form3.status = 1
+		form3.save()
+		
+		savedTodos = Todo.objects.all()
+		
+		#consider if today is dec 22
+		today = datetime.strptime('20141222', '%Y%m%d' )
+		#update all waiting todos date that are before today to today
+		savedTodos.filter(status=1).filter(date__lt=today).update(date=today)
+
+		self.assertEqual(savedTodos.count(), 3)
+		#make sure this is the right entry we are checking by doing these asserts
+		self.assertEqual(savedTodos[0].title, 'sample title one')
+		self.assertEqual(savedTodos[0].summary, 'sample summary one')
+		self.assertEqual(savedTodos[0].status, 1)
+		#its date should be converted to dec 22, check if that is the case
+		self.assertEqual(savedTodos[0].date.strftime('%Y%m%d'), '20141222')
