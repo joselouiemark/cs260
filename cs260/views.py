cb96bbb22836c98a0f9418226e4da6e4a0811946
Jose Louie Mark <joselouiemark@gmail.com>
2014-12-19 22:54:10 +0800

    Added register functions to view.

--- cs260/views.py
+++ cs260/views.py
@@ -9,3 +9,19 @@ def login(request):
 	c.update(csrf(request))
 	c.update({'title':'Login'})
 	return render_to_response('login.html',c)
+	
+def register_user(request):
+	if request.method == 'POST':
+		form = MyRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/accounts/register_success')
+	
+	args = {}
+	
+	args['form'] = MyRegistrationForm()
+	
+	return render_to_response('register.html', args)
+
+def register_success(request):
+	return render_to_response('register_success.html')
