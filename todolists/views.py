9b925f3ae5ef1b624a0fba185e3d6e91e9d674ad
Jose Louie Mark <joselouiemark@gmail.com>
2014-12-20 00:29:02 +0800

    Added todo list html

--- todolists/views.py
+++ todolists/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
+from django.template import Context
+from django.views.generic.base import TemplateView
+from todolists.models import Todo
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from datetime import datetime
+from datetime import date
 
 # Create your views here.
 def todolist(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    if not request.user.is_authenticated():
+		return HttpResponseRedirect('/accounts/login/')
+	
+	allObjs = Todo.objects.filter(owner=request.user.username);
+	
+	if stat > 0 and stat < 4:
+		allObjs = allObjs.filter(status=stat)
+		
+	return render_to_response('todolist.html',
+							{'todolist':allObjs,'full_name': request.user.first_name +" " +request.user.last_name,
+							'today':date.today()})
