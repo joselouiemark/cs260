ac91c40fb320f177123c9b17f5809c2e73a894ca
Jose Louie Mark <joselouiemark@gmail.com>
2014-12-19 22:15:56 +0800

    Added models

--- todolists/models.py
+++ todolists/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+	owner = models.CharField(max_length=50)
+	title = models.CharField(max_length=200)
+	summary = models.TextField()
+	date = models.DateField('date published')
+	status = models.IntegerField()
