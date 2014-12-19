9ea1ed835c59965d912eab1e1a04d2bb2d0f0bf1
Jose Louie Mark <joselouiemark@gmail.com>
2014-12-20 03:16:31 +0800

    Based on functional and unit tests, revised urls and views.

--- todolists/urls.py
+++ todolists/urls.py
@@ -3,4 +3,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^all/$', 'todolists.views.todolist'),
+	url(r'^all/(?P<stat>\d+)/$', 'todolists.views.todolist'),
+	url(r'^all/(?P<stat>\d+)/(?P<dfrom>\w+)/(?P<dto>\w+)/$', 'todolists.views.todolist'),
+	url(r'^todo/(?P<todo_id>\d+)/$', 'todolists.views.todo'),
+	url(r'^create/$','todolists.views.create'),
+	url(r'^cancel/(?P<todo_id>\d+)/$', 'todolists.views.cancel_todo'),
+	url(r'^finish/(?P<todo_id>\d+)/$', 'todolists.views.finish_todo'),
 )
--- todolists/views.py
+++ todolists/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.template import Context
 from django.views.generic.base import TemplateView
 from todolists.models import Todo
+from todolists.forms import TodoForm
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from datetime import datetime
@@ -24,3 +25,66 @@ def todolist(request, stat=0, dfrom='', dto=''):
 	return render_to_response('todolist.html',
 							{'todolist':allObjs,'full_name': request.user.first_name +" " +request.user.last_name,
 							'today':date.today()})
+							
+def create(request):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect('/accounts/login/')
+		
+	if request.POST:
+		form = TodoForm(request.POST)
+		form.owner = request.user.username
+		if form.is_valid():
+			form.save()
+			
+			return HttpResponseRedirect('/todolists/all')
+	else:
+		form = TodoForm()
+		form.owner = request.user.username
+	
+	return render_to_response('create_todo.html',  {
+		'form': form,
+		'full_name': request.user.first_name +" " +request.user.last_name,
+		}, context_instance=RequestContext(request))
+
+def todo(request, todo_id = None):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect('/accounts/login/')
+		
+	todo = Todo.objects.get(id=todo_id)
+	if request.POST:
+		form = TodoForm(request.POST, instance=todo)
+		form.owner = request.user.username
+		if form.is_valid():
+			form.save()
+			
+			return HttpResponseRedirect('/todolists/all')
+	else:
+		form = TodoForm(instance=todo)
+		form.owner = request.user.username
+	
+	return render_to_response('todo.html',  {
+		'form': form,
+		'full_name': request.user.first_name +" " +request.user.last_name
+		}, context_instance=RequestContext(request))
+	
+def cancel_todo(request, todo_id):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect('/accounts/login/')
+		
+	if todo_id:
+		a = Todo.objects.get(id=todo_id)
+		a.status = 3
+		a.save()
+		
+	return HttpResponseRedirect('/todolists/all')
+
+def finish_todo(request, todo_id):
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect('/accounts/login/')
+		
+	if todo_id:
+		a = Todo.objects.get(id=todo_id)
+		a.status = 2
+		a.save()
+		
+	return HttpResponseRedirect('/todolists/all')
